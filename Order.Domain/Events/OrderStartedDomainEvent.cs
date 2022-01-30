fab252d71abfbccc6836b7949ab778d686c68ab8
Recep Alışkan <recep.aliskan@devatek.com>
2022-01-30 18:17:32 +0300

    Add project files.

---
+++ Order.Domain/Events/OrderStartedDomainEvent.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order.Domain.Events
+{
+    public class OrderStartedDomainEvent : INotification // orderın başladıgını buyer ve ilgili taraflara bildiren event
+    {
+        public AggregateModels.OrderModels.Order Order { get; set; }
+        public string UserName { get; set; }
+
+        public OrderStartedDomainEvent(string userName, AggregateModels.OrderModels.Order order)
+        {
+            UserName = userName;
+            Order = order;
+        }
+    }
+}
